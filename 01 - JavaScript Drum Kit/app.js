8cdae31e135a62b8042640986cac48be47a7a0c2
S <picotech@pm.me>
2022-03-15 20:39:19 -0400

    add key press

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,10 +1,19 @@
 window.addEventListener("keydown", function (event) {
 
     const audio = document.querySelector("audio[data-key=" + '"' + event.key + '"' + "]");
-    const key = document.querySelector("key[data-key=" + '"' + event.key + '"' + "]");
+    const key = document.querySelector(".key[data-key=" + '"' + event.key + '"' + "]");
 
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+
+    key.classList.add("playing")
 });
 
+for (let i = 0; i < querySelectorAll(".key").length; i++) {
+    document.querySelectorAll(".key")[i].addEventListener("transitioned", removeTransition);
+}
+
+
+const allKeys = document.querySelectorAll(".key");
+allKeys
